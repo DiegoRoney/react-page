7a303ff0e20e4d818794416143e153752ba2088f
DiegoRoney <46231316+DiegoRoney@users.noreply.github.com>
2021-04-03 08:04:27 -0300

    2

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
-import {useState} from 'react';
+import { getFontDefinitionFromNetwork} from 'next/dist/next-server/server/font-utils';
+import { useState} from 'react';
 
 function home() {
-    return <div>HOME 2</div>
+    return <div>
+        <h1> HOME </h1>
+
+        <p> teste de react </p>
+        <a href="https://diegoroney.github.io/Diego/index.html"> @Diego </a>
+
+    </div>
 }
 
 export default home
-
